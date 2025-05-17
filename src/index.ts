b6b107692c29b9b2a6cec7fb27ff5b7c7d082663
manvardhan singh <manvardhansingh05@gmail.com>
2025-05-18 00:16:52 +0530

    ported the complied code to ts

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,8 @@ function clearDebug() {
 document.getElementById("runButton").addEventListener("click", function () {
     try {
         clearDebug();
-        const inputCode = document.getElementById("input").value;
+        // const inputCode = document.getElementById("input").value;
+        const inputCode = (document.getElementById("input") as HTMLInputElement).value;
         updateDebug(`Input code: ${inputCode}`);
 
         const compiledCode = compiler(inputCode);
@@ -28,12 +29,17 @@ document.getElementById("runButton").addEventListener("click", function () {
         updateDebug(`Execution result: ${executionResult}`);
 
         // Display the execution result in the output textarea
-        const outputElement = document.getElementById("output");
+        // const outputElement = document.getElementById("output");
+        // outputElement.value = executionResult !== null ? executionResult : "Error during code execution";
+
+        const outputElement = document.getElementById("output") as HTMLInputElement;
         outputElement.value = executionResult !== null ? executionResult : "Error during code execution";
 
     } catch (error) {
         console.error("Error:", error);
         updateDebug(`Error: ${error.message}`);
-        document.getElementById("output").value = `Error: ${error.message}`;
+        // document.getElementById("output").value = `Error: ${error.message}`;
+        const outputElement = document.getElementById("output") as HTMLInputElement;
+        outputElement.value = `Error: ${error.message}`;
     }
 }); 
--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,22 @@
 import express from 'express';
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
+import path from 'path';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+// import { fileURLToPath } from 'url';
+
+// const __filename = fileURLToPath(import.meta.url);
+
+
+// __dirname in TypeScript (CommonJS workaround)
+const __dirname = path.resolve();
 
 const app = express();
 
 // Set the correct MIME type for JavaScript modules
 app.use((req, res, next) => {
-    if (req.url.endsWith('.js')) {
-        res.setHeader('Content-Type', 'application/javascript');
-    }
-    next();
+  if (req.url.endsWith('.js')) {
+    res.setHeader('Content-Type', 'application/javascript');
+  }
+  next();
 });
 
 // Serve static files from the current directory
